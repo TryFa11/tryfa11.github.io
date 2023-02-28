96fefcb41742559a2eff8564a767b3b8f84660b9
TryFa11 <110195726+TryFa11@users.noreply.github.com>
2023-02-27 18:20:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ var query = document.getElementById("results");
 var response;
 document.getElementById("send").addEventListener("click", function(){
     var inp = document.querySelector("input").value;
+    var imgdata = document.getElementById("input2");
+    const formdata = new FormData();
+    imgdata.addEventListener("change", ev => {
+        formdata.append(imgdata, ev.target.files[0]);
+        fetch("https://api.imgbb.com/1/upload", {
+            method: 'POST',
+            headers: {
+                Authorization: "c6d96c681f4c4f161a7bda3b217264de"
+            },body:formdata
+        }).then(data=>data.json().then(data=>console.log(data)));
+    });
+
     fetch('https://CloudBackend.tryfall.repl.co/p', {
     method: 'POST',
     headers: {
@@ -10,7 +22,6 @@ document.getElementById("send").addEventListener("click", function(){
     },
     body: JSON.stringify({ "content": inp })
 })
-.then(response => response.json())
-.then(response => {
-    query.innerText = 'QueryID: '+response.status
+.then(response => response.json()).then(response => {
+    query.innerText = response.status
 })});
